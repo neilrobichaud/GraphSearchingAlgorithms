285099a3d0c08d6aa237e73353fed79ebf747d26
Neil Robichaud <neil.robichaud@hotmail.com>
2016-03-24 15:45:52 -0400

    added sets

---
+++ src/cas2xb3_A3_robichaud_NR/BinarySearch.java
@@ -0,0 +1,42 @@
+package cas2xb3_A3_robichaud_NR;
+
+import java.util.ArrayList;
+
+public class BinarySearch {
+	
+    public static int indexOf(Restaurant[] a, double key) {	//key is input lat
+        int lo = 0;
+        int mid = -1;
+        int hi = a.length - 1;
+        while (lo <= hi) {            
+            mid = lo + (hi - lo) / 2;
+            if      (key < a[mid].lat) hi = mid - 1;			// will return index of closest latitude restaurant
+            else if (key > a[mid].lat) lo = mid + 1;
+            else return mid;
+        }
+        return mid;												//returns mid  even if key is not found
+    }
+    
+    public static ArrayList<Restaurant> getlist(double restaurantlat, double restaurantlng, int closestlatindex){
+    	ArrayList<Restaurant> closeLatList= new ArrayList<Restaurant>();
+    	int i = closestlatindex;
+    	while (Math.abs(FindPath.rlist[i].lat - restaurantlat) < 0.5){		//adds all restaurant within -0.5 lat points
+    		closeLatList.add(FindPath.rlist[i]);
+    		i--;
+    	}
+    	i= closestlatindex;
+    	while (Math.abs(FindPath.rlist[i].lat - restaurantlat) < 0.5){		//adds all restaurant withing +0.5 lat point
+    		closeLatList.add(FindPath.rlist[i]);
+    		i++;
+    	}    	
+    	
+    	for (int j = 0; j<closeLatList.size(); j++){
+    		if (Math.abs(closeLatList.get(j).lng - restaurantlng)>0.5){		// if the difference between is bigger than 0.5 remove from list
+    			closeLatList.remove(j);
+    		}    		
+    	}
+		return closeLatList;
+    	
+    }
+    
+}
--- src/cas2xb3_A3_robichaud_NR/City.java
+++ src/cas2xb3_A3_robichaud_NR/City.java
@@ -17,12 +17,9 @@ public class City {
 		this.adjList = new ArrayList<City>();
 	}
 	
-	public static Restaurant[] nearbyRestaurants(double lat, double lng){
-		//binary search on restaurant list
-		// binarySearch(this.lat,this.lng) --> returns index of closest restaurant in restaurant list
+	public ArrayList<Restaurant> nearbyRestaurants(){
 		int closestlatindex = BinarySearch.indexOf(FindPath.rlist, lat);
-		
-		return null;
+		return BinarySearch.getlist(lat, lng, closestlatindex);		
 	}
 	
 	
