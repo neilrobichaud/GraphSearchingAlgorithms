ce1b756ca780ddda34a67861cb3b3be38d8fbcb0
Neil Robichaud <neil.robichaud@hotmail.com>
2016-03-25 12:17:46 -0400

    added methods to find a city and read cities and edges into graph

--- src/cas2xb3_A3_robichaud_NR/Edge.java
+++ src/cas2xb3_A3_robichaud_NR/Edge.java
@@ -3,7 +3,7 @@ package cas2xb3_A3_robichaud_NR;
 public class Edge {
 	private City v;
 	private City w;
-	private double weight;
+	double weight;
 	
 	public Edge(City v, City w, double weight){
 		this.v = v;
--- src/cas2xb3_A3_robichaud_NR/FindPath.java
+++ src/cas2xb3_A3_robichaud_NR/FindPath.java
@@ -10,16 +10,25 @@ public class FindPath {
 	public static ArrayList<String> BKmeals;
 	public static ArrayList<String> Wmeals;
 	public static Restaurant[] rlist;
+	public static ArrayList<City> citylist;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Restaurant> bklist = readFromFile("2XB3_A3_DataSets/burgerking.csv");
 		ArrayList<Restaurant> mcdlist = readFromFile("2XB3_A3_DataSets/mcdonalds.csv");
 		ArrayList<Restaurant> wendyslist = readFromFile("2XB3_A3_DataSets/wendys.csv");
+		
 		ArrayList<Restaurant> allRList = new ArrayList<Restaurant>();
 		allRList.addAll(bklist);
 		allRList.addAll(mcdlist);
 		allRList.addAll(wendyslist);
 		rlist = allRList.toArray(new Restaurant[allRList.size()]);
+		citylist = new ArrayList<City>();
+		citylist = readCityFromFile("2XB3_A3_DataSets/zips1990.csv");
+		
+		readEdgeFromFile("2XB3_A3_DataSets/connectedCities.txt");
+		addEdge(cityfinder("Boston"),cityfinder("el paso"),3.0);
+		System.out.print(cityfinder("el paso").adjList.get(0).weight);
+		//System.out.print(cityfinder("Boston").adjList.get(0).weight);
 		
 	}
 	
@@ -47,6 +56,89 @@ public class FindPath {
 			e.printStackTrace();
 		}
 		return current;//.toArray(new Restaurant[current.size()]);											//returns the arraylist
+	}
+	public static ArrayList<City> readCityFromFile(String filename){
+		Scanner input;
+		ArrayList<City> current = new ArrayList<City>();
+		try {
+			input = new Scanner(new File(filename));
+			input.nextLine();																				//skip first row of headers
+			while (input.hasNextLine()) {
+				String k = input.nextLine();
+				String[] l= k.split(",");
+				String[] state = {l[2]};
+				if (cityfinder(l[3])==null){	// if city is not already in citylist
+				City a = new City(Double.parseDouble(l[4]), Double.parseDouble(l[5]), l[3], state);
+				current.add(a);
+				}
+			}
+
+			input.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return current;//.toArray(new Restaurant[current.size()]);											//returns the arraylist
+	}
+	public static void readEdgeFromFile(String filename) {		//method to read from the input file, returns each line as an string in an arraylist
+		Scanner input;
+		int open;
+		int closed;
+		Boolean first;
+		ArrayList<City> current = new ArrayList<City>();
+		try {
+			input = new Scanner(new File(filename));
+			
+			while (input.hasNextLine()) {
+				String k = input.nextLine();
+				first=true;
+				open=0;
+				closed=0;
+				for (int i=0; i<k.length();i++){
+					if (k.charAt(i)=='('){
+						open++;
+					}
+					else if(k.charAt(i)==')'){
+						closed++;
+					}
+					
+					if (open == 1 && closed == 1 && first==true){
+						String firstcity=k.substring(0,i+1);
+						String secondcity=k.substring(i+3);
+						String[] tuple1 = firstcity.split("\\(");
+						String[] tuple2 = secondcity.split("\\(");
+						tuple1[0].trim();
+						tuple1[1]=tuple1[1].substring(0, tuple1[1].length()-1);
+						tuple2[0].trim();
+						tuple2[1]=tuple2[1].substring(0, tuple2[1].length()-1);
+
+						//addEdge(cityfinder(tuple1[0]),cityfinder(tuple2[0]),2);
+						//System.out.println(tuple2[0]);
+						//System.out.println(secondcity);	
+						first=false;
+					}
+				}
+
+			}
+
+			input.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//return current;//.toArray(new Restaurant[current.size()]);											//returns the arraylist
+	}
+	/*
+	 * takes a string and returns a city from citylist
+	 */
+	public static City cityfinder(String x){
+		for (int i=0;i<citylist.size();i++){
+			if (citylist.get(i).name.equalsIgnoreCase(x)){
+				//System.out.print(x);
+				return citylist.get(i);
+			}
+		}
+		return null;
 	}
 
 }
